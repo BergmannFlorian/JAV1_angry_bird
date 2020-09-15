b76f9159c16c06b79d2bdef1b49ac972a1d0b5a8
BERGMANN Florian <Florian.BERGMANN@cpnv.ch>
2020-09-15 14:52:58 +0200

    create voc class

---
+++ core/src/ch/cpnv/sit1a/models/data/Word.java
@@ -0,0 +1,16 @@
+package ch.cpnv.sit1a.models.data;
+
+public class Word {
+    private String value1;
+    private String value2;
+    private boolean allocated = false;
+    private boolean found = false;
+
+    private Word(String value1, String value2){
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+
+    public String getValue1(){return value1;}
+    public String getValue2(){return value2;}
+}
---
+++ core/src/ch/cpnv/sit1a/providers/VocProvider.java
@@ -0,0 +1,4 @@
+package ch.cpnv.sit1a.providers;
+
+public class VocProvider {
+}
