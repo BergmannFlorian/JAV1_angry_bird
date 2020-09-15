b33f244f559904093ef39d09ac89edfb4db0f020
BERGMANN Florian <Florian.BERGMANN@cpnv.ch>
2020-09-15 16:06:24 +0200

    add voc provider

--- core/src/ch/cpnv/sit1a/models/data/Word.java
+++ core/src/ch/cpnv/sit1a/models/data/Word.java
@@ -6,7 +6,7 @@ public class Word {
     private boolean allocated = false;
     private boolean found = false;
 
-    private Word(String value1, String value2){
+    public Word(String value1, String value2){
         this.value1 = value1;
         this.value2 = value2;
     }
