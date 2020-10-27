c874119f86000879b96bad90b01667d787fee75d
Florian Bergmann <florian.bergmann@cpnv.ch>
2020-10-27 14:49:04 +0100

    add stack to screen

--- core/src/ch/cpnv/sit1a/Angry_Bird.java
+++ core/src/ch/cpnv/sit1a/Angry_Bird.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 
+import java.util.Stack;
+
 import ch.cpnv.sit1a.activities.Play;
 import ch.cpnv.sit1a.activities.Welcome;
 
@@ -13,22 +15,38 @@ public class Angry_Bird extends ApplicationAdapter{
 	private static ApplicationAdapter playActivity;
 	private static ApplicationAdapter welcomeActivity;
 
+	private static Stack<ApplicationAdapter> screens;
+
 	@Override
 	public void create () {
 		playActivity = new Play();
 		welcomeActivity = new Welcome();
 		activity = welcomeActivity;
+
+		screens = new Stack<>();
+		screens.push(playActivity);
 	}
 	@Override
 	public void render () {
 		activity.render();
+//		screens.peek().render();
 	}
 
 	public static void changeActivity(activities toActivity){
 		if(toActivity == activities.play)activity = playActivity;
-		else if(toActivity == activities.welcome)activity = welcomeActivity;
+		if(toActivity == activities.welcome)activity = welcomeActivity;
 		Gdx.input.setInputProcessor((InputProcessor) activity);
 	}
+
+	public static void openActivity(activities toActivity){
+		if(toActivity == activities.play)screens.push(playActivity);
+		if(toActivity == activities.welcome)screens.push(welcomeActivity);
+		Gdx.input.setInputProcessor((InputProcessor) screens.peek());
+	}
+	public static void closeActivity(){
+		screens.pop();
+	}
+
 	public enum activities{
 		play,
 		welcome
